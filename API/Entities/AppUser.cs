633748beb0ba11832502fa0c7b9545520d408f31
Mariano Arias <arias.mariano.ar@gmail.com>
2025-07-01 22:10:29 -0300

    add EF

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,7 +2,7 @@
 {
     public class AppUser
     {
-        public int Id { get; set; }
+        public int Id { get; set; } //para EF debe ser siempre ID, igual creeria que agregarse un decorador posible ser puede
         public required string UserName { get; set; }
     }
 }
