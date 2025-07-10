2457b1f6aa169131ded4ff87deb71d0a77c12cc4
Mariano Arias <arias.mariano.ar@gmail.com>
2025-07-10 11:31:00 -0300

    added CORS configuration to the API

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 //builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -24,6 +25,7 @@ var app = builder.Build();
 //app.UseHttpsRedirection();
 
 //app.UseAuthorization();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
