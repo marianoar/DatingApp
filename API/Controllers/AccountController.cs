d9bcff2a540e548f0c329fded65345e6617fd997
Mariano Arias <arias.mariano.ar@gmail.com>
2025-07-19 20:40:06 -0300

    added atuthentication middleware and extension method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
@@ -8,10 +10,10 @@ using System.Text;
 
 namespace API.Controllers
 {
-    public class AccountController(AppDbContext context): BaseApiController
+    public class AccountController(AppDbContext context, ITokenService tokenService): BaseApiController
     {
         [HttpPost("register")] // api/account/register
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
 
             if(await EmailExists(registerDto.Email))
@@ -31,11 +33,11 @@ namespace API.Controllers
             context.Users.Add(user); //no se usa async aqui, solo en casos especiales
             await context.SaveChangesAsync();
 
-            return Ok(user);
+            return user.ToDto(tokenService);
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await context.Users.SingleOrDefaultAsync(x=> x.Email == loginDto.Email);
             if (user == null)
@@ -49,8 +51,8 @@ namespace API.Controllers
                 if (computedHash[i] != user.PasswordHash[i])
                     return Unauthorized("Invalid password");
             }
-            return Ok(user);
 
+            return user.ToDto(tokenService);
         }
         private async Task<bool> EmailExists(string email)
         {
