3a27250ec88e73c1d1186f666ee65c1c6add1494
Mariano Arias <arias.mariano.ar@gmail.com>
2025-10-16 15:18:15 -0300

    Created Member Repository Pattern

---
+++ API/Data/MemberRepository.cs
@@ -0,0 +1,36 @@
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class MemberRepository(AppDbContext context) : IMemberRepository
+    {
+        public async Task<IReadOnlyList<Member>> GetMemberAsync()
+        {
+            return await context.Members.ToListAsync();
+        }
+
+        public async Task<Member?> GetMemberByIdAsync(string id)
+        {
+            return await context.Members.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<Photo>> GetPhotosForMemberAsync(string memberId)
+        {
+            return await context.Members.Where(w=>w.Id == memberId)
+                                        .SelectMany(w=>w.Photos)
+                                        .ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(Member member)
+        {
+            context.Entry(member).State = EntityState.Modified;
+        }
+    }
+}
---
+++ API/Interfaces/IMemberRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IMemberRepository
+    {
+        void Update(Member member);
+        Task<bool> SaveAllAsync();
+        Task<IReadOnlyList<Member>> GetMemberAsync();
+        Task<Member?> GetMemberByIdAsync(string id);
+        Task<IReadOnlyList<Photo>> GetPhotosForMemberAsync(string memberId);
+    }
+}
