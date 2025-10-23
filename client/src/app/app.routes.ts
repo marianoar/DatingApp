522c188dda99bc433cffa7ae7fb193ee54c6dfb9
Mariano Arias <arias.mariano.ar@gmail.com>
2025-10-23 10:37:17 -0300

    Creating member detail page and child components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,6 +8,9 @@ import { authGuard } from '../core/guards/auth-guard';
 import { TestErrors } from '../features/test-errors/test-errors';
 import { NotFound } from '../shared/errors/not-found/not-found';
 import { ServerError } from '../shared/errors/server-error/server-error';
+import { MemberProfile } from '../features/members/member-profile/member-profile';
+import { MemberPhotos } from '../features/members/member-photos/member-photos';
+import { MemberMessages } from '../features/members/member-messages/member-messages';
 
 export const routes: Routes = [
   {
@@ -27,6 +30,13 @@ export const routes: Routes = [
       {
         path: 'members/:id',
         component: MemberDetail,
+        children:[
+          {path: '', redirectTo: 'profile', pathMatch: 'full' },
+          {path: 'profile', component: MemberProfile, title: 'Profile' },
+          {path: 'photos', component: MemberPhotos, title: 'Photos' },
+          {path: 'messages', component: MemberMessages, title: 'Messages' },
+
+        ]
       },
       {
         path: 'lists',
--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MemberService } from '../../../core/services/member-service';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { filter, Observable } from 'rxjs';
+import { Member } from '../../../types/member';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
-export class MemberDetail {
+export class MemberDetail implements OnInit {
 
+  private memberService = inject(MemberService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  protected title = signal<string | undefined>('Profile');
+  protected member$?: Observable<Member>
+
+  ngOnInit(): void {
+
+    this.member$ = this.loadMember();
+    this.title.set(this.route.firstChild?.snapshot?.title);
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      next: () => this.title.set(this.route.firstChild?.snapshot?.title)
+    }
+    )
+  }
+
+  loadMember() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (!id)
+      return;
+    return this.memberService.getMember(id);
+  }
 }
